77ebbff2c60f5e130b83b8826c6deb434c3da0c4
aurbanow <aurbanow@pjwstk.edu.pl>
2024-12-17 15:19:59 +0100

    minor fixes

--- Westeros/westeros-diagnostics-tools/src/main/java/com/westeros/DiagnosticsConfiguration.java
+++ Westeros/westeros-diagnostics-tools/src/main/java/com/westeros/DiagnosticsConfiguration.java
@@ -1,8 +1,7 @@
 package com.westeros;
 
 import com.westeros.diagnostics.services.IDiagnosticsServiceClient;
-import com.westeros.tools.schedulers.Scheduler;
-import com.westeros.tools.schedulers.SchedulerThread;
+
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,13 +11,10 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 @Slf4j
 public class DiagnosticsConfiguration {
-    private final SchedulerThread schedulerThread;
     private final IDiagnosticsServiceClient diagnosticsServiceClient;
-    private final Scheduler scheduler;
 
     @PostConstruct
     public void runScheduler(){
-
 
     }
 }
